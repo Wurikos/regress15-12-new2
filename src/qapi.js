efa51af2999606fcd95e9ad728365cea0d3c6765
Wurikos <50768680+Wurikos@users.noreply.github.com>
2020-12-15 09:56:29 +0300

    Changes sm

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fd8516c3f0ee3001e1d38ff",
+				"5fd8516c3f0ee3001e1d3901"
+			]
+		},
+		"5fd8516c3f0ee3001e1d38ff": {
+			"id": "5fd8516c3f0ee3001e1d38ff",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fd8516c3f0ee3001e1d3901": {
+			"id": "5fd8516c3f0ee3001e1d3901",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
